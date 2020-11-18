d2252563688ab7b374ad3165ef1eddd35d5f70ca
zhangjl16 <zhangjl16@asiainfo.com>
2020-11-18 15:34:36 +0800

    新增服务

--- src/main/java/com/github/xabgesagtx/example/Controller/CloudSeeController.java
+++ src/main/java/com/github/xabgesagtx/example/Controller/CloudSeeController.java
@@ -1,6 +1,6 @@
 package com.github.xabgesagtx.example.Controller;
 
-import com.github.xabgesagtx.example.Service.DealCloudSee;
+import com.github.xabgesagtx.example.Service.DealCloudSee2;
 import com.github.xabgesagtx.example.utils.FileUtils;
 import com.github.xabgesagtx.example.utils.MyResponse;
 import org.slf4j.Logger;
@@ -42,7 +42,7 @@ public class CloudSeeController {
     private Long SLEEP_TIME;
 
     @Autowired
-    private DealCloudSee cloudSee;
+    private DealCloudSee2 cloudSee;
 
 
     @GetMapping(value = "/download")
@@ -60,13 +60,11 @@ public class CloudSeeController {
 
     }
 
-    @Async
     @GetMapping(value = "/scan")
     public MyResponse scanner(HttpServletRequest request){
         MyResponse myResponse= new MyResponse(200,true);
         String startName = request.getParameter("fileName");
         if (!StringUtils.isEmpty(startName)){
-            ReentrantLock lock = new ReentrantLock();
             String startHead = startName.substring(0,1);
             String[] tmp = startName.split(";");
             Integer allCount = 0;
@@ -81,47 +79,9 @@ public class CloudSeeController {
             Integer endNUm = startNum+allCount;
             List<String> result = new CopyOnWriteArrayList<>();
             long startTime=System.currentTimeMillis();
-            for (;startNum<=endNUm;startNum++){
-                try {
-                    cloudSee.execute(result,startHead+startNum);
-                    Thread.sleep(SLEEP_TIME);
-                } catch (InterruptedException e) {
-                    try {
-                        Thread.sleep(20);
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                    logger.info(e.getMessage());
-                }
-                lock.lock();
-                if (result.size()>=10){
-                    logger.info("已扫描超过10个，先写入文件，最后扫描值为："+startNum);
-                    FileUtils.FileWriteListforTure(cloudPath+startHead+endNUm+".txt",result);
-                }
-                lock.unlock();
-				/*	resp = httpsPost.get(url+startHead+startNum);
-					if ("200".equals(String.valueOf(resp.statusCode()))){
-						logger.info(startHead+startNum+"："+resp.body());
-						//logger.info(JSONObject.parseObject(resp.body()).getString("onln"));
-						if(!StringUtils.isEmpty(JSONObject.parseObject(resp.body()).getString("onln"))){
-						if (JSONObject.parseObject(resp.body()).getString("onln").equals("1")){
-							result.add(startHead+startNum);
-						}
-						}
-					}*/
-					/*if (result.size()>10){
-						logger.info("已扫描超过10个，先写入文件，最后扫描值为："+startNum);
-						fileUtils.FileWriteListforTure(FILE_PATH+startHead+endNUm+".txt",result);
-						result.clear();
-					}*/
-            }
+            cloudSee.execute(result,startHead,startNum,endNUm,SLEEP_TIME,cloudPath);
             long endtime=System.currentTimeMillis();
             logger.info(("程序运行时间： "+(endtime-startTime)+"ms"));
-            logger.info("已扫描完成，写入文件，最后扫描值为："+startNum);
-            FileUtils.FileWriteListforTure(cloudPath+startHead+endNUm+".txt",result);
-            result.clear();
-
-
         }else {
             myResponse.setCode(9999);
             myResponse.setSuccess(false);
